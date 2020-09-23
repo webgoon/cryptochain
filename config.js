793dd819f631a69288d8f620a7790c20df22e520
Benjamin Carter <webgoonie@gmail.com>
2020-09-23 11:56:59 -0400

    backend done

--- config.js
+++ config.js
@@ -12,8 +12,14 @@ const GENESIS_DATA = {
 
 const STARTING_BALANCE = 1000;
 
+const REWARD_INPUT = { address: '*authorized-reward*' };
+
+const MINING_REWARD = 50;
+
 module.exports = { 
   GENESIS_DATA, 
   MINE_RATE,
-  STARTING_BALANCE
+  STARTING_BALANCE,
+  REWARD_INPUT,
+  MINING_REWARD
  };
--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
+const TransactionMiner = require('./app/transaction-miner');
 
 const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain, transactionPool, wallet });
+const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wallet, pubsub });
 
 const DEFAULT_PORT = 7000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -68,6 +70,22 @@ app.post('/api/transact', (req, res) => {
 app.get('/api/transaction-pool-map', (req, res) => {
   res.json(transactionPool.transactionMap);
 });
+
+app.get('/api/mine-transactions', (req, res) => {
+  transactionMiner.mineTransactions();
+
+  res.redirect('/api/blocks');
+});
+
+app.get('/api/wallet-info', (req, res) => {
+  const address = wallet.publicKey;
+
+  res.json({
+    address,
+    balance: Wallet.calculateBalance({ chain: blockchain.chain, address })
+  });
+});
+
 
 // To synchrnoize with the peer and accept who has the longest chain.
 const syncWithRootState = () => {
